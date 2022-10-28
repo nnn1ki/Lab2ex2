f93a9a9c25391bdc4b8ba2513b96fd85f010eb93
Your Name <you@example.com>
2022-10-28 21:53:44 +0800

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace SecondLab2ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFirst = new System.Windows.Forms.TextBox();
+            this.textSecond = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Входит ли превое слово во второе?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(21, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите первое слово: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(21, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите второе слово: ";
+            // 
+            // textFirst
+            // 
+            this.textFirst.Location = new System.Drawing.Point(272, 60);
+            this.textFirst.Multiline = true;
+            this.textFirst.Name = "textFirst";
+            this.textFirst.Size = new System.Drawing.Size(218, 27);
+            this.textFirst.TabIndex = 3;
+            // 
+            // textSecond
+            // 
+            this.textSecond.Location = new System.Drawing.Point(272, 108);
+            this.textSecond.Multiline = true;
+            this.textSecond.Name = "textSecond";
+            this.textSecond.Size = new System.Drawing.Size(218, 29);
+            this.textSecond.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(24, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(466, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Проверить!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.labelAnswer.Location = new System.Drawing.Point(22, 242);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(111, 33);
+            this.labelAnswer.TabIndex = 6;
+            this.labelAnswer.Text = "Ответ: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 345);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textSecond);
+            this.Controls.Add(this.textFirst);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Вхождение первого слова";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFirst;
+        private System.Windows.Forms.TextBox textSecond;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelAnswer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecondLab2ex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //ввод переменных от пользователя
+                string first = this.textFirst.Text;
+                string second = this.textSecond.Text;
+                
+                Logic ans = new Logic(first, second);
+
+                this.labelAnswer.Text = "Ответ: " + ans.search();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+        }
+
+        //TODO 
+        //отремонтировать ввод через энтер
+
+        private void Form1_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+    }
+
+    public class Logic
+    {
+
+        private string first;
+        private string second;
+
+        public Logic(string ln1, string ln2)
+        {
+            first = ln1;
+            second = ln2;
+
+            first = deleteSimilar(first);
+            second = deleteSimilar(second);
+        }
+
+
+        private string deleteSimilar(string line)
+        {
+            string ans = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                bool inLine = false;
+
+                for (int j = i + 1; j < line.Length; j++)
+                {
+                    if (line[i] == line[j])
+                    {
+                        inLine = true;
+                        break;
+                    }
+                }
+
+                if (inLine == false)
+                {
+                    ans += line[i];
+                }
+            }
+
+            return ans;
+        }
+
+        public string search()
+        {
+            string ans = "";
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                bool included = false;
+
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if (first[i] == second[j])
+                    {
+                        included = true;
+                        break;
+                    }
+                }
+
+                ans += check(included);
+            }
+
+            return ans;
+        }
+
+        public string check(bool expression)
+        {
+            if (expression) return "да ";
+            else return "нет ";
+        }
+
+    }
+
+
+}
